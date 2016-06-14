c64a409db76fef0f6f1ca8c504df14a074d5bf1c
Jack Stein <jacklsteiniv@gmail.com>
2016-06-14 14:39:11 -0700

    Set up UI routing; need to deploy to GH-pages to test.

---
+++ js/app.js
@@ -0,0 +1,36 @@
+angular
+.module('ElmApp', ['ui.router', 'ngAnimate'])
+.config(MainRouter)
+
+function MainRouter($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+
+  //set up states - urls and templates
+  .state('intro', {
+    url: '/',
+    templateUrl: 'intro.html'
+
+  })
+
+  .state('chapter1', {
+    url: '/chapter1',
+    templateUrl: 'chapter1.html'
+
+
+  })
+
+  .state('chapter2', {
+    url: '/chapter2',
+    templateUrl: 'chapter2.html'
+
+  })
+
+  .state('chapter3', {
+    url: '/chapter3',
+    templateUrl: 'chapter3.html'
+  })
+
+  $urlRouterProvider.otherwise('/');
+
+}
